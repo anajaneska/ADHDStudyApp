8eea9b5f5f917d612c6a8bfb5cc099000f7dd87d
anajaneska <lindalea202@gmail.com>
2025-10-14 19:10:19 +0200

    added spring security with jwt

---
+++ backend/src/main/java/mk/ukim/finki/backend/model/User.java
@@ -0,0 +1,22 @@
+package mk.ukim.finki.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String  username;
+    private String password;
+
+}
---
+++ backend/src/main/java/mk/ukim/finki/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package mk.ukim.finki.backend.repository;
+
+import mk.ukim.finki.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+}
