a9ba79e5843361be63cfc680d30c09dee4bf3042
anajaneska <lindalea202@gmail.com>
2025-10-16 18:40:43 +0200

    moved frontend into main repo structure

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Redirect, Route, Routes} from "react-router-dom";
+import Login from './components/Login-Register/login.js'
+import Register from './components/Login-Register/register.js';
+import HomePage from './pages/homepage.js';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: !!localStorage.getItem("jwt"),
+    };
+  }
+
+  onLogin = () => {
+    this.setState({ isLoggedIn: true }, this.loadTrips);
+  };
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login onLogin={this.onLogin} />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/components/Login-Register/login.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import instance from '../../custom-axios/axios';
+import {useNavigate} from "react-router-dom";
+import './login.css'
+
+export default function Login() {
+  const [form, setForm] = useState({ username: '', password: '' });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await instance.post('/auth/login', form);
+      const token = response.data;
+      console.log("Received JWT token:", token);
+
+      localStorage.setItem('jwt', token);
+      alert("Logged in successfully");
+      navigate(`/`);
+
+      setForm({ username: '', password: '' });
+    } catch (err) {
+      alert("Login failed");
+      console.error("Login error:", err);
+    }
+  };
+
+  return (
+    <div className={'container px-6'}>
+     <h2 className={'my-3'}>Login</h2>
+    <form onSubmit={handleSubmit}>
+
+      <input
+        type="text"
+        placeholder="Username"
+        value={form.username}
+        onChange={(e) => setForm({ ...form, username: e.target.value })}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={form.password}
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+        required
+      />
+      <button type="submit">Login</button>
+    </form>
+    </div>
+  );
+}
